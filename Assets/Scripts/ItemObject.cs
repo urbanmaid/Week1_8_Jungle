dc3648ad0ed493077ba67ef691cd761081a8018f
urbanmaid <70835212+urbanmaid@users.noreply.github.com>
2025-03-04 22:56:51 +0900

    스킬 아이템 생성

--- Assets/Scripts/ItemObject.cs
+++ Assets/Scripts/ItemObject.cs
@@ -7,6 +7,7 @@ public class ItemObject : MonoBehaviour
     Item Code: 
     0 for Heal
     1 for Missile
+    2 for Skill Usage
     */
 
     private float rotSpeed = 8f;
@@ -47,8 +48,36 @@ public class ItemObject : MonoBehaviour
                 //gm.ChargeMissileAmount();
                 Debug.Log("Missile amount has been increased into " + missileAmount);
                 break;
+            case 2:
+                UseSkill();
+                Debug.Log("Skill has been used.");
+                break;
             default:
                 Debug.LogError("Invalid item code detected, no effect applied.");
+                break;
+        }
+    }
+
+    public void UseSkill()
+    {
+        // Skill usage
+        int skillMode = Random.Range(0, 3);
+        switch (skillMode)
+        {
+            case 0:
+                // Gravity Shot
+                Debug.Log("Skill 1 has been used.");
+                break;
+            case 1:
+                // Shield
+                Debug.Log("Skill 2 has been used.");
+                break;
+            case 2:
+                // Rampage
+                Debug.Log("Skill 3 has been used.");
+                break;
+            default:
+                Debug.LogError("Invalid skill mode detected, no effect applied.");
                 break;
         }
     }
